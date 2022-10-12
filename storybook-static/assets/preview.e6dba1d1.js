ce627a71e1c92148fd6225f651e8244797b8edd2
Richard <richard.beletatti@gmail.com>
2022-10-12 19:46:07 -0300

    Initial commit

---
+++ storybook-static/assets/preview.e6dba1d1.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.a3c232d1.js";import"./iframe.df8417e9.js";import"./es.map.constructor.c68e86ae.js";import"./es.number.to-fixed.d0d41222.js";import"./index.a6794fa0.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.e6dba1d1.js.map
